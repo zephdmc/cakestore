36833a01857f0aedd51e92a6ef508d5f4846d372
zephdmc <zephdmc@gmail.com>
2025-08-20 14:28:01 +0100

    Update HomePage.jsx
    bg-greengrade

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -298,7 +298,7 @@ useEffect(() => {
       style={{ fontFamily: "calibri" }}
       className=" bg-white block md:flex justify-between  sm:mx-[2%] -mt-2 md:-mt-12 md:mx-[10%] text-black md:shadow-md"
     >
-      <div className="flex lg:flex sm:block md:justify-between  mx-12 md:mx-2 text-mghso-10 m-4 ">
+      <div className="flex lg:flex sm:block md:justify-between bg-greengrade  mx-12 md:mx-2 text-mghso-10 m-4 ">
         <div className="p-2">
           <BiBookReader className="text-[30px] text-gray-700" />
         </div>
@@ -312,7 +312,7 @@ useEffect(() => {
           </p>
         </div>
       </div>
-      <div className="flex lg:flex sm:block  md:justify-between  mx-12 md:mx-2 text-mghso-10 m-4 ">
+      <div className="flex lg:flex sm:block bg-greengrade  md:justify-between  mx-12 md:mx-2 text-mghso-10 m-4 ">
         <div className="p-2">
           <FaGraduationCap className="text-[30px] text-gray-700" />
         </div>
@@ -325,7 +325,7 @@ useEffect(() => {
           </p>
         </div>
       </div>
-      <div className="flex lg:flex sm:block  md:justify-between m-4  mx-12 md:mx-2 text-mghso-10 ">
+      <div className="flex lg:flex sm:block bg-greengrade md:justify-between m-4  mx-12 md:mx-2 text-mghso-10 ">
         <div className="p-2">
           <GiTrophyCup className="text-[30px] text-gray-700" />
         </div>
@@ -338,7 +338,7 @@ useEffect(() => {
           </p>
         </div>
       </div>
-      <div className=" flex lg:flex sm:block  md:justify-between m-4  mx-12 md:mx-2 text-mghso-10 ">
+      <div className=" flex lg:flex sm:block bg-greengrade md:justify-between m-4  mx-12 md:mx-2 text-mghso-10 ">
         <div className="p-2">
           <IoPeopleOutline className="text-[30px] text-gray-700" />
         </div>
@@ -349,7 +349,7 @@ useEffect(() => {
           </p>
         </div>
       </div>
-      <div className="flex lg:flex sm:block  md:justify-between m-4 mx-12 md:mx-2 text-mghso-10 ">
+      <div className="flex lg:flex sm:block bg-greengrade md:justify-between m-4 mx-12 md:mx-2 text-mghso-10 ">
         <div className="p-2">
           <GiTwoFeathers className="text-[30px] text-gray-700" />
         </div>
