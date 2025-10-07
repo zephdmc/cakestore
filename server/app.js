4c127ae3e4fb297683e14b7520c8b5646979fe10
zephdmc <zephdmc@gmail.com>
2025-10-07 15:47:06 +0100

    Update app.js
    wewe

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD');
   res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', 'false');
   res.header('Access-Control-Max-Age', '86400');
   
   if (req.method === 'OPTIONS') {
