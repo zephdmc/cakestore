d024647dc6586f4a99199cf8dd313b72350aaa1a
zephdmc <zephdmc@gmail.com>
2025-10-07 12:19:02 +0100

    Update HomePage.jsx
    df

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import ProductCard from '../../components/products/ProductCard';
 import { getProducts } from '../../services/productServic';
 import TestimonialSlider from '../../pages/home/HomePageComponent/TestimonialSlider';
+import AboutSection from '../../pages/home/HomePageComponent/Aboutsection';
 import { FiHeart, FiAward, FiLoader, FiAlertTriangle, FiShoppingBag, FiArrowRight, FiStar } from 'react-icons/fi';
 import { FaWhatsapp } from 'react-icons/fa';
 import { useAuth } from '../../context/AuthContext';
