24f34c1a1d810030abfedeadbd83e198920d7695
Ezeji Zephrinus <zephdmc@gmail.com>
2025-10-31 13:23:49 +0100

    Update MugsContext.jsx
    erer

--- client/src/context/MugsContext.jsx
+++ client/src/context/MugsContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState, useCallback } from 'react';
-import { getProducts } from '../services/productService';
+import { getProducts } from '../services/mugsservice';
 
 const ProductContext = createContext();
 
